1b4facefaa610713025c9a8da77ca5b12787ef04
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-06 15:55:57 +0530

    pro

--- src/main/java/com/nest/productapp2_backend/controller/ProductController.java
+++ src/main/java/com/nest/productapp2_backend/controller/ProductController.java
@@ -4,9 +4,12 @@ package com.nest.productapp2_backend.controller;
 import com.nest.productapp2_backend.dao.ProductDao;
 import com.nest.productapp2_backend.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -21,5 +24,11 @@ public class ProductController {
         System.out.println(p.getPname().toString());
         dao.save(p);
         return "product the add";
+    }
+
+    @GetMapping("/view")
+    public List<Product> View()
+    {
+        return (List<Product>) dao.findAll();
     }
 }
