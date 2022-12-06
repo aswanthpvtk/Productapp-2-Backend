8542b0a3dd355a133d8f7d7785f79c281ef05158
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-06 15:29:58 +0530

    pro

---
+++ src/main/java/com/nest/productapp2_backend/Productapp2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.productapp2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Productapp2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Productapp2BackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nest/productapp2_backend/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.nest.productapp2_backend.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+
+    @PostMapping("/add")
+    public String ProductAdd()
+    {
+
+    }
+}
